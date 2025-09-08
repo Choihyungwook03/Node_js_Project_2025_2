aa9efe98401049c9c765c32f3d379cad70c2bd94
Choihyungwook03 <ejyong1212@g.shingu.ac.kr>
2025-09-08 12:20:50 +0900

    [Feat] 기본 통신 연결 테스트

---
+++ Node/Hello.js
@@ -0,0 +1,9 @@
+let http = require("http");
+
+http.createServer(function (request, responge)
+{
+    responge.writeHead(200, {'content-type' : 'text/plain'});
+
+    responge.end("Hello world");
+}).listen(8000);
+console.log("Server running at http://127.0.0.1:8000");
