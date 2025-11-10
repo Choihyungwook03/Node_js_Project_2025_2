612e2122e54f2981d9fa092a397579b19cd124a0
Choihyungwook03 <ejyong1212@g.shingu.ac.kr>
2025-11-10 11:55:42 +0900

    [Feat] DB => 서버 => 클라이언트 데이터 전달 테스트

---
+++ Node/GameServer.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+const app = express();
+
+app.use(express.json());
+
+const pool = mysql.createPool({
+    host : 'localhost',
+    user : 'root',
+    password : 'wtre9483@@',
+    database : 'gametest'
+});
+
+app.post('/login', async (req, res)=>{
+    const {username, password_hash} = req.body;
+
+    try{
+        const [player] = await pool.query(
+          'SELECT * FROM player WHERE username = ? AND password_hash = ?',
+          [username, password_hash]  
+        );
+
+        if(player.length > 0){
+            await pool.query(
+                'UPDATE player SET last_login = CURRENT_TIMESTAMP WHERE player_id = ?',
+                [player[0].player_id]
+            );
+        }
+        else{
+            res.status(500).json({success: false, message : '로그인 실패'});
+        }
+    }
+    catch(error){
+        res.status(500).json({success: false, message : error.message});
+    }
+});
+
+app.get('/inventory/:playerId', async (req, res) => {
+    try{
+        const[inventory] = await pool.query(
+            'SELECT inv.item_id, inv.quantity FROM inventories inv JOIN items i ON inv.item_id = i.item_id WHERE inv.player_id = ?',
+            [req.params.playerId]
+        );
+        res.json(inventory);
+    }
+    catch(error){
+        res.status(500).json({success: false, message : error.message});
+    }
+});
+
+app.get('/quests/:playerId', async(req, res) => {
+    try{
+        const[quests] = await pool.query(
+            'SELECT q.*, pq.`STATUS` FROM player_quests pq JOIN quests q ON pq.quest_id = q.quest_id WHERE pq.player_id = ?',
+            [req.params.playerId]
+        );
+        res.json(quests);
+    }
+    catch(error){
+        res.status(500).json({success: false, message : error.message});
+    }
+});
+
+const PORT = 3000;
+
+app.listen(PORT, ()=> {
+    console.log(`서버 실행 중 : ${PORT}`);
+})
