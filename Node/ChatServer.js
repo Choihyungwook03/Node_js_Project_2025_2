abc03e27cbd55fb370f21102f84c6ffe451db522
Choihyungwook03 <ejyong1212@g.shingu.ac.kr>
2025-11-24 11:29:47 +0900

    [Feat] 플레이어 위치, 회전 동기화

--- Node/ChatServer.js
+++ Node/ChatServer.js
@@ -19,7 +19,8 @@ class GameServer {
 
             this.players.set(playerId, {
                 socket : socket,
-                position: {x:0, y:0, z:0}
+                position: {x:0, y:0, z:0},
+                rotation: {x:0, y:0, z:0}
             });
             console.log(`클라이언트 접속! ID : ${playerId}, 현재 접속자 : ${this.clients.size}`);
 
@@ -29,6 +30,20 @@ class GameServer {
                 message : '서버에 연결 되었습니다!'
             };
 
+            this.players.forEach((player, pid) => {
+                if(pid !== playerId)
+                {
+                    const joinMsg = {
+                        type: 'playerJoin',
+                        playerId: pid,
+                        position: player.position,
+                        rotation: player.rotation
+                    };
+                    socket.send(JSON.stringify(joinMsg));
+                    console.log(`기존 플레이어 정보 전송 : ${pid} -> ${playerId}`);
+                }
+            });
+
             socket.send(JSON.stringify(welcomeData));
 
             socket.on('message', (message) =>  
@@ -36,13 +51,41 @@ class GameServer {
                 try
                 {
                     const data = JSON.parse(message);
-                    console.log('수신된 메시지 : ', data);
 
-                    this.broadcast({
+                    if(data.type === 'chat')
+                    {
+                        console.log('수신된 메시지 : ', data);
+
+                        this.broadcast({
                         type: 'chat',
                         playerId: playerId,
                         message: data.message
-                    });
+                        });
+                    }
+                    else if(data.type === 'positionUpdate')
+                    {
+                        const player = this.players.get(playerId);
+                        if (player)
+                        {
+                            if(data.position)
+                            {
+                                player.position = data.position;
+                            }
+                            if(data.rotation)
+                            {
+                                player.rotation = data.rotation;
+                            }
+                        }
+
+                        const updateMsg = {
+                            type: 'positionUpdate',
+                            playerId: playerId,
+                            position: player.position,
+                            rotation: player.rotation
+                        };
+
+                        this.broadcast(updateMsg, socket);
+                    }
                 }
                 catch
                 {
@@ -63,15 +106,24 @@ class GameServer {
             });
         });
     }
-    broadcast(data)
+    broadcast(data, excludeSocket = null)
     {
         const message = JSON.stringify(data);
-        this.clients.forEach(clients => {
-            if(clients.readyState === WebSocket.OPEN)
-            {
-                clients.send(message);
-            }
-        });
+        let sentCount = 0;
+
+        this.clients.forEach(client => {
+        if (client !== excludeSocket && client.readyState === WebSocket.OPEN) 
+        {
+            client.send(message);  
+            sentCount++;
+        }
+});
+
+
+        if (data.type !== 'positionUpdate')
+        {
+            console.log(`브로드 캐스트 완료 ${data.type} (${sentCount} 명에게 전송)`);
+        }
     }
     generatePlayerId()
     {
